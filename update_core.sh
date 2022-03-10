bfda87ccf1da601776e44bb00bf119a69038b2e9
G. Blake Meike <blake.meike@couchbase.com>
2022-03-10 12:48:39 -0800

    Revert Core submodule dependency

--- update_core.sh
+++ update_core.sh
@@ -3,101 +3,70 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR="${SCRIPT_DIR}/.."
 
-NEXUS_URL="http://nexus.build.couchbase.com:8081/nexus/content/repositories/releases/com/couchbase/litecore"
-
-
 function usage() {
     echo "Usage: $0 <core commit> <ee commit>"
     exit 1
 }
 
 function fail() {
-    popd > /dev/null
-    exit 67
+    echo "Aborting!!!"
+    exit 1
 }
 
-if [ "$#" -lt 1 -o "$#" -gt 2 ]; then
-    usage
-fi
+if [ "$#" -ne 2 ]; then usage; fi
 
-CORE_COMMIT="$1"
-if [ -z "$CORE_COMMIT" ]; then
-    usage
-fi
+CE_COMMIT=$1
+if [ -z "${CE_COMMIT}" ]; then usage; fi
 
-CORE_EE_COMMIT="$2"
+EE_COMMIT=$2
+if [ -z $EE_COMMIT ]; then usage; fi
 
+
+# Make sure that Core and EE Core are in a reasonable state
 pushd "${ROOT_DIR}" > /dev/null
-
 cd core
 if [[ $(git status -s --ignore-submodules=dirty) != '' ]]; then
     echo "Error: Core is dirty"
     fail
 fi
 
-if [[ ! -z "$CORE_EE_COMMIT" ]]; then
-    if [[ $(git status -s --ignore-submodules=dirty) != '' ]]; then
-        echo "Error: Core-EE is dirty"
-        fail
-    fi
-fi
-
-cd ..
-
-echo "=== Update core"
-cd core
-head=`git rev-parse HEAD`
-git checkout prev 2> /dev/null || git checkout -b prev
-git reset --hard $head
-git remote prune origin
-git fetch
-git checkout "$CORE_COMMIT" || fail
-
-if [[ ! -z "$CORE_EE_COMMIT" ]]; then
-    echo "=== Update core-EE"
-    cd  ../couchbase-lite-core-EE
-    head=`git rev-parse HEAD`
-    git checkout prev 2> /dev/null || git checkout -b prev
-    git reset --hard $head
-    git remote prune origin
-    git fetch
-    git checkout "$CORE_EE_COMMIT" || fail
-fi
-
-cd ..
-${SCRIPT_DIR}/verify_core.sh
-if [[ ! $? -eq 0 ]]; then
-    cd core
-    git checkout -b prev
-
-    if [[ ! -z "$CORE_EE_COMMIT" ]]; then
-        cd  ../couchbase-lite-core-EE
-        git checkout -b prev
-    fi
-
-    echo "Error: incomplete core build"
+cd ../couchbase-lite-core-EE
+if [[ $(git status -s --ignore-submodules=dirty) != '' ]]; then
+    echo "Error: EE Core is dirty"
     fail
 fi
+cd ..
 
-echo "=== Fetch new core"
+# Verify that the artifacts exist
+"${SCRIPT_DIR}/verify_core.sh" $CE_COMMIT $EE_COMMIT
+if [[ $? -ne 0 ]]; then fail; fi
+
+# Update Core
 cd core
+HEAD=`git rev-parse HEAD`
+echo "=== Update Core: $HEAD -> $CE_COMMIT"
+git checkout stable 2> /dev/null || git checkout -b stable
+git reset --hard $HEAD
+git remote prune origin
+git fetch
 git checkout working 2> /dev/null || git checkout -b working
-git reset --hard "$CORE_COMMIT"
+git reset --hard $CE_COMMIT || fail
 git submodule update --recursive
 
-if [[ ! -z "$CORE_EE_COMMIT" ]]; then
-    cd  ../couchbase-lite-core-EE
-    git checkout working 2> /dev/null || git checkout -b working
-    echo "git reset --hard $CORE_EE_COMMIT"
-    git reset --hard "$CORE_EE_COMMIT"
-    git submodule update --recursive
-fi
-
-echo "=== Fetch"
-cd ..
-rm -rf common/lite-core
-common/tools/fetch_litecore.sh -e EE -n "${NEXUS_URL}"
-common/tools/fetch_android_litecore.sh -e EE -n "${NEXUS_URL}"
+# Update EE Core
+cd  ../couchbase-lite-core-EE
+HEAD=`git rev-parse HEAD`
+echo "=== Update EE Core: $HEAD -> $EE_COMMIT"
+git checkout stable 2> /dev/null || git checkout -b stable
+git reset --hard $HEAD
+git remote prune origin
+git fetch
+git checkout working 2> /dev/null || git checkout -b working
+git reset --hard "$EE_COMMIT" || fail
+git submodule update --recursive
 
 popd > /dev/null
+
+echo "=== Fetch new core"
+${SCRIPT_DIR}/fetch_core.sh
 
--- verify_core.sh
+++ verify_core.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 NEXUS_URL="http://nexus.build.couchbase.com:8081/nexus/content/repositories/releases/com/couchbase/litecore"
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-TOOLS_DIR="${SCRIPT_DIR}/../common/tools"
 STATUS=0
+
+function usage() {
+    echo "Usage: $0 <core commit> <ee commit>"
+    exit 1
+}
 
 test_core() {
    OS=$1
    EDITION=$2
-   SHA=$3
+   ID=$3
    SUFFIX=$4
    echo -n "${OS} ${EDITION}: "
-   curl -I -s -f -o /dev/null "${NEXUS_URL}/couchbase-litecore-${OS}/${SHA}/couchbase-litecore-${OS}-${SHA}.${SUFFIX}" -o "${OS}-${EDITION}.${SUFFIX}"
+   curl -I -s -f -o /dev/null "${NEXUS_URL}/couchbase-litecore-${OS}/${ID}/couchbase-litecore-${OS}-${ID}.${SUFFIX}" -o "${OS}-${EDITION}.${SUFFIX}"
    if [ $? -eq 0 ]; then
       echo "Succeeded"
    else
@@ -20,29 +23,35 @@ test_core() {
    fi
 }
 
+
+if [ "$#" -ne 2 ]; then usage; fi
+
+CE_COMMIT=$1
+if [ -z "${CE_COMMIT}" ]; then usage; fi
+
+EE_COMMIT=$2
+if [ -z $EE_COMMIT ]; then usage; fi
+
+# calculate the artifact IDs
+CE_ID=$CE_COMMIT
+EE_ID=`echo -n ="${CE_COMMIT}${EE_COMMIT}" | shasum -a 1`
+EE_ID=${EE_ID:0:40}
+
 rm -rf .core-tmp
 mkdir .core-tmp
 pushd .core-tmp > /dev/null
 
-echo -n "CE SHA: "
-"${TOOLS_DIR}/litecore_sha.sh" -e CE -o .core-sha
-CE_SHA=`cat .core-sha`
-
-echo -n "EE SHA: "
-"${TOOLS_DIR}/litecore_sha.sh" -e EE -o .core-sha
-EE_SHA=`cat .core-sha`
-
-test_core "linux" "CE" "${CE_SHA}" "tar.gz"
-test_core "linux" "EE" "${EE_SHA}" "tar.gz"
+test_core "linux" "CE" "${CE_ID}" "tar.gz"
+test_core "linux" "EE" "${EE_ID}" "tar.gz"
 
 for OS in macosx windows-win64; do
-   test_core "${OS}" "CE" "${CE_SHA}" "zip"
-   test_core "${OS}" "EE" "${EE_SHA}" "zip"
+   test_core "${OS}" "CE" "${CE_ID}" "zip"
+   test_core "${OS}" "EE" "${EE_ID}" "zip"
 done
 
 for ABI in armeabi-v7a arm64-v8a x86 x86_64; do
-   test_core "android-${ABI}" "CE" "${CE_SHA}" "zip"
-   test_core "android-${ABI}" "EE" "${EE_SHA}" "zip"
+   test_core "android-${ABI}" "CE" "${CE_ID}" "zip"
+   test_core "android-${ABI}" "EE" "${EE_ID}" "zip"
 done
 
 popd > /dev/null
