fca6c2f5f8b730cc6e87d1d29463663e76aa21f9
G. Blake Meike <blake.meike@couchbase.com>
2022-03-31 12:56:44 -0700

    New core updater works with the new staging branch strategy

--- update_core.sh
+++ update_core.sh
@@ -3,27 +3,9 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR="${SCRIPT_DIR}/.."
 
-function usage() {
-    echo "Usage: $0 <core commit> <ee commit>"
-    exit 1
-}
-
-function fail() {
-    echo "Aborting!!!"
-    exit 1
-}
-
-if [ "$#" -ne 2 ]; then usage; fi
-
-CE_COMMIT=$1
-if [ -z "${CE_COMMIT}" ]; then usage; fi
-
-EE_COMMIT=$2
-if [ -z $EE_COMMIT ]; then usage; fi
-
+pushd "${ROOT_DIR}" > /dev/null
 
 # Make sure that Core and EE Core are in a reasonable state
-pushd "${ROOT_DIR}" > /dev/null
 cd core
 if [[ $(git status -s --ignore-submodules=dirty) != '' ]]; then
     echo "Error: Core is dirty"
@@ -37,36 +19,46 @@ if [[ $(git status -s --ignore-submodules=dirty) != '' ]]; then
 fi
 cd ..
 
-# Verify that the artifacts exist
-"${SCRIPT_DIR}/verify_core.sh" $CE_COMMIT $EE_COMMIT
-if [[ $? -ne 0 ]]; then fail; fi
-
 # Update Core
 cd core
+git fetch
 HEAD=`git rev-parse HEAD`
+CE_COMMIT=`git rev-parse origin/staging/master`
 echo "=== Update Core: $HEAD -> $CE_COMMIT"
 git checkout stable 2> /dev/null || git checkout -b stable
 git reset --hard $HEAD
-git remote prune origin
-git fetch
 git checkout working 2> /dev/null || git checkout -b working
 git reset --hard $CE_COMMIT || fail
 git submodule update --recursive
+git remote prune origin
 
 # Update EE Core
 cd  ../couchbase-lite-core-EE
+git fetch
 HEAD=`git rev-parse HEAD`
+EE_COMMIT=`git rev-parse origin/master`
 echo "=== Update EE Core: $HEAD -> $EE_COMMIT"
 git checkout stable 2> /dev/null || git checkout -b stable
 git reset --hard $HEAD
-git remote prune origin
-git fetch
 git checkout working 2> /dev/null || git checkout -b working
 git reset --hard "$EE_COMMIT" || fail
 git submodule update --recursive
+git remote prune origin
+
+echo "=== Update Artifacts"
+cd  ..
+rm -rf ./.penv
+python3 -m venv ./.penv
+. ./.penv/bin/activate
+pip -q install GitPython
+"${ROOT_DIR}/common/tools/clean_litecore.sh"
+python core/scripts/fetch_litecore_version.py -x "${ROOT_DIR}/etc" --ee -v macos android -r core -o "${ROOT_DIR}/common/lite-core"
+deactivate
+rm -rf ./.penv
+
+# Temporary: we need this until the build scripts use the new artifact downloader
+cd core
+git checkout HEAD^2
 
 popd > /dev/null
-
-echo "=== Fetch new core"
-${SCRIPT_DIR}/fetch_core.sh
 
