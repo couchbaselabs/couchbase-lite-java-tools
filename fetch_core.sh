0dc42081be158149031841d5c52b9e0fc2a0233b
G. Blake Meike <blake.meike@couchbase.com>
2023-01-25 10:33:50 -0800

    Delete lite-core dir before fetch

--- fetch_core.sh
+++ fetch_core.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+COMMON_DIR="${SCRIPT_DIR}/../common"
 TOOLS_DIR="${SCRIPT_DIR}/jenkins"
+
+rm -rf "${COMMON_DIR}/lite-core"
 
 "${TOOLS_DIR}/fetch_core.sh" -d -e EE -p android
 "${TOOLS_DIR}/fetch_core.sh" -d -e EE 
